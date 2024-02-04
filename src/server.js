9be6a1c6e4d599a65e4afc8301f516df7d9a22c4
“Kamran <kamran.afridi120@hotmail.com>
2024-02-04 19:20:49 +0100

    ; colon added in server.js file

--- src/server.js
+++ src/server.js
@@ -9,5 +9,5 @@ var server = http.createServer(function(request, response) {
 
 var port = 3000;
 server.listen(port);
-module.exports = server
+module.exports = server;
 console.log("Server is available on Port:", port);
